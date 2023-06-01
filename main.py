578c885b252c4c44f443df3d3dd9ca85f9d915d8
devfle <52854338+devfle@users.noreply.github.com>
2023-06-02 00:45:36 +0200

    fix: reduce space between progress bar

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def generate_content(readme_instance: ReadmeLevel, start_section: str, end_secti
 
     return (f"{start_section}\n"
             f"{ getenv('INPUT_CARD_TITLE') if getenv('INPUT_CARD_TITLE') else '' } \n"
-            f"<pre>level: { user_level }  \
+            f"<pre>level: { user_level }\
             { draw_progress_bar(to_next_lvl) } {round(to_next_lvl, 2)}%</pre>\n"
             f"{ ep_information if getenv('INPUT_SHOW_EP_INFO') else '' }"
             f"{end_section}")
