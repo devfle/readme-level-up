22f26e4a3ab0a6c84841f80c3165cd431543e676
devfle <52854338+devfle@users.noreply.github.com>
2023-06-04 15:09:48 +0000

    tests: add test for readme level class

--- tests/test_readme_level.py
+++ tests/test_readme_level.py
@@ -3,6 +3,7 @@ Unit Tests for readme_level.py
 """
 
 import unittest
+from unittest.mock import patch
 from readme_level import ReadmeLevel
 
 class TestLevelSystem(unittest.TestCase):
@@ -22,6 +23,21 @@ class TestLevelSystem(unittest.TestCase):
 
         level_data = readme_instance.percentage_ep_level(-5, 1000)
         self.assertEqual(level_data, -0.5)
+
+    @patch('readme_level.ReadmeLevel._update_user_data')
+    def test_calc_current_ep(self, mock_update_user_data):
+        """Tests the ep calc"""
+        mock_update_user_data.return_value = None
+        readme_instance: ReadmeLevel = ReadmeLevel()
+
+        readme_instance.contribution_count = 120
+        readme_instance.project_count = 0
+        readme_instance.discussion_count = 0
+        readme_instance.follower_count = 12
+
+        readme_instance.calc_current_ep()
+        self.assertEqual(readme_instance.current_ep, 2700)
+
 
 if __name__ == '__main__':
     unittest.main()
